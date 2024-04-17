4f264ac74ea66c4e3fdeb0f04f38696a8f206d04
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-04-17 11:05:07 -0400

    s10c1 depart carrousel

---
+++ carrousel.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Carrousel plugin
+ * Author: Salma  Bourakkadi
+ * Description: Affiche le carrousel associé  une galerie de wordpress
+ * Version: 1.7.2
+ * Author URI: https://github.com/2025242/4w4-2024
+ * Author URI: https://referenced.ca
+ */
+
+function enqueue_style_script(){
+$version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style(   'em_plugin_carrousel_css',
+                     plugin_dir_url(__FILE__) . "style.css",
+                     array(),
+                     $version_css);
+
+    wp_enqueue_script(  'em_plugin_carrousel_js',
+                    plugin_dir_url(__FILE__) ."js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_style_script' );
+ 
+function genere_html() {
+$html='
+<button class="bouton__ouvrir">Ouvrir Carousel</button>
+<div class="carrousel">
+    <a href="" class="carrousel__x">X</a>
+    <figure class="carrousel__figure"></figure>
+    <form action="" class="carrousel__form"></form>
+</div>
+';
+return $html;
+}
+
+add_shortcode( 'carrousel', 'genere_html' );
