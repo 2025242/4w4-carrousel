a56ed94a935dcb434bee90480fbe2bcd7b29f40a
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-04-18 12:03:35 -0400

    s10c2 depart d'ajout d'img de galerie vers le carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,10 +3,30 @@
 // On récupère le bouton pour ouvrir et le carrousel
     let carrousel = document.querySelector('.carrousel')
     let bouton = document.querySelector('.bouton__ouvrir')
- 
+    let galerie = document.querySelector('.galerie')
+    let carrousel__figure = document.querySelector('.carrousel__figure')
+    //creation dinamique d'une img du carrousel
+    let carrousel__img = document.createElement('img')
+    carrousel__img.classList.add('carrousel__img')
+    /*recupere la premiere img de la galerie*/
+    //let galerie__img = galerie.querySelector('img')
+    //pour cree une colletion d'img de la galerie
+    let galerie__img = galerie.querySelectorAll('img')
+    console.log(galerie__img)
+
+    for (const elm of galerie__img)
+    {
+        console.log(elm.src)
+    }
+    console.log("premiere img de la galerie ="+ galerie__img.src)
+    carrousel__img.src= galerie__img.src
+    console.log("premiere img de du carousel ="+ carrousel__img.src)
+    carrousel__figure.appendChild(carrousel__img)
+    console.log( carrousel__figure)
+  
     // On ajoute un écouteur d'événement sur le bouton pour ouvrir le carrousel
     bouton.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir')
+        carrousel.classList.add('carrousel--ouvrir')// permer de ouvrir le carrousel
     })
  
     // On récupère le bouton pour fermer le carrousel
@@ -14,6 +34,7 @@
  
     // On ajoute un écouteur d'événement sur le bouton pour fermer le carrousel
     boutonFermer.addEventListener('mousedown', function(){
-        carrousel.classList.remove('carrousel--ouvrir')
+        carrousel.classList.remove('carrousel--ouvrir')// fermer le carrousel
     })
+
 })()
